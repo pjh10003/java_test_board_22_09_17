eb10c0f9e2c1c13b74ca3eda76f3ef98fc33b541
pjh10003 <pjh10003@nate.com>
2023-10-15 12:48:44 +0900

    시작 알림 메세지 출력1.1

--- src/com/sbs/exam/board/Main.java
+++ src/com/sbs/exam/board/Main.java
@@ -3,7 +3,7 @@ package com.sbs.exam.board;
 public class Main {
   public static void main(String[] args) {
 
-    System.out.println("== 게시판 v 0.1 ==");
+    System.out.println("== 게시판 v 0.1.1 ==");
     System.out.println("== 프로그램 시작 ==");
     System.out.println("명령 )");
     System.out.println("== 프로그램 종료  ==");
