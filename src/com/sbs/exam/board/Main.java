324db1bce036dbb837283a5a08a768733cb9d64d
Park_jong_ho <pjh10003@nate.com>
2023-10-15 19:20:09 +0900

    게시물 리스트에서 출력을 최신순으로

--- src/com/sbs/exam/board/Main.java
+++ src/com/sbs/exam/board/Main.java
@@ -52,7 +52,8 @@ public class Main {
             System.out.println("번호 / 제목");
             System.out.println("-------------------");
 
-            for(Article article : articles){
+            for(int i = articles.size()-1  ;i >= 0;i--){
+              Article article = articles.get(i);
               System.out.printf("%d /%s\n",article.id,article.title);
             }
 
