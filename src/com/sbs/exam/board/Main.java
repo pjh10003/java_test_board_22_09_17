fde90f21d24d980678f8d2202d6b6cec270118b8
pjh10003 <pjh10003@nate.com>
2023-10-15 13:29:25 +0900

    새 게시물 입력받기 기능 구현시작

--- src/com/sbs/exam/board/Main.java
+++ src/com/sbs/exam/board/Main.java
@@ -9,20 +9,42 @@ public class Main {
 
     System.out.println("== 게시판 v 0.1.1 ==");
     System.out.println("== 프로그램 시작 ==");
-    System.out.printf("명령 ) ");
+
 
 
     while(true)
     {
-
+      System.out.printf("명령 ) ");
       String cmd = sc.nextLine();
-      System.out.printf("입력된 명령어 = %S\n" ,cmd);
-      if(cmd.equals("exit")) {
-        break;
+
+
+
+          if(cmd.equals("exit")) {
+
+            break;
+
+          }else if(cmd.equals("/usr/article/write")){
+
+            System.out.println("== 게시물 등록 ==");
+
+            System.out.printf("제목 : ");
+            String title = sc.nextLine();
+
+            System.out.printf("내용 : ");
+            String body = sc.nextLine();
+
+            int id = 1;
+            System.out.printf("%d 번 게시물이 등록되었습니다.\n",id);
+
+          }else {
+            System.out.printf("입력된 명령어 = %S\n", cmd);
+          }
+
       }
+
+    System.out.println("== 프로그램 종료  ==");
+    sc.close();
     }
 
-   System.out.println("== 프로그램 종료  ==");
-    sc.close();
+
   }
-}
